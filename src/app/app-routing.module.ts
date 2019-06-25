078a1ae3579d09761680661dc819f28e0b0391c3
francisco.mazali <franciscomazali@gmail.com>
2019-06-25 15:30:16 -0300

    adding nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
-import { CategoryFormComponent } from './pages/categories/category-form/category-form.component';
 
+const routes: Routes = [{
+  path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule'
+}];
 
-const routes: Routes = [
-  { path: '', component: CategoryListComponent },
-  { path: 'new', component: CategoryFormComponent },
-  { path: ':id/edit', component: CategoryFormComponent },
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoriesModule } from "./pages/categories/categories.module";
+import { CategoriesModule } from './pages/categories/categories.module';
 
 @NgModule({
   declarations: [
